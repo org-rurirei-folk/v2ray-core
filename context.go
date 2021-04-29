91babe349c9dd0b7419c06662dfea4b0023a7ebd
rurirei <72071920+rurirei@users.noreply.github.com>
2021-04-29 20:50:17 +0800

    revert: conflicts on context.go

--- context.go
+++ context.go
@@ -61,19 +61,3 @@ func mustToContext(ctx context.Context, v *Instance) context.Context {
 	}
 	return ctx
 }
-
-// ToContext returns ctx from the given context, or creates an Instance if the context doesn't find that.
-func ToContext(ctx context.Context, v *Instance) context.Context {
-	if FromContext(ctx) != v {
-		ctx = context.WithValue(ctx, v2rayKey, v)
-	}
-	return ctx
-}
-
-// MustToContext returns ctx from the given context, or panics if not found that.
-func MustToContext(ctx context.Context, v *Instance) context.Context {
-	if c := ToContext(ctx, v); c != ctx {
-		panic("V is not in context.")
-	}
-	return ctx
-}
