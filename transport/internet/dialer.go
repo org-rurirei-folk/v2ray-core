d7e547f31fa2680b442e12dd35aee92ca4492c0e
rurirei <72071920+rurirei@users.noreply.github.com>
2021-04-30 02:59:37 +0800

    Update dialer.go

--- transport/internet/dialer.go
+++ transport/internet/dialer.go
@@ -72,10 +72,11 @@ func DialSystem(ctx context.Context, dest net.Destination, sockopt *SocketConfig
 
 	var src net.Destination
 	if outbound := session.OutboundFromContext(ctx); outbound != nil {
-		src = net.Destination{Address: outbound.Gateway}
-	}
-	if inbound := session.InboundFromContext(ctx); inbound != nil {
-		src = inbound.Source
+		src.Address = outbound.Gateway
+		src.Network = dest.Network
+		if inbound := session.InboundFromContext(ctx); inbound != nil {
+			src.Port = inbound.Source.Port
+		}
 	}
 
 	if transportLayerOutgoingTag := session.GetTransportLayerProxyTagFromContext(ctx); transportLayerOutgoingTag != "" {
