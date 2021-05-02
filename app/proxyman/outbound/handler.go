bddfc8ae6026c0599cf8cf288a8671a80b9bd0e6
rurirei <72071920+rurirei@users.noreply.github.com>
2021-05-02 23:41:51 +0800

    Update handler.go

--- app/proxyman/outbound/handler.go
+++ app/proxyman/outbound/handler.go
@@ -163,11 +163,8 @@ func (h *Handler) Address() net.Address {
 
 // Dial implements internet.Dialer.
 func (h *Handler) Dial(ctx context.Context, dest net.Destination) (internet.Connection, error) {
-	outbound := session.OutboundFromContext(ctx)
-	if outbound == nil {
-		outbound = new(session.Outbound)
-		ctx = session.ContextWithOutbound(ctx, outbound)
-	}
+	outbound := new(session.Outbound)
+	ctx = session.ContextWithOutbound(ctx, outbound)
 	// outbound.Gateway is non-null for internet.DialSystem
 	outbound.Gateway = net.AnyDestination(dest.Network)
 
