cd8e8fc7c91fc29b0977bcd4fe4796d45076cffd
rurirei <72071920+rurirei@users.noreply.github.com>
2021-05-03 22:11:09 +0800

    Update decode.go

--- common/geodata/decode.go
+++ common/geodata/decode.go
@@ -18,6 +18,7 @@ import (
 	"google.golang.org/protobuf/encoding/protowire"
 
 	"github.com/v2fly/v2ray-core/v4/common/errors"
+	"github.com/v2fly/v2ray-core/v4/common/platform/filesystem"
 )
 
 //go:generate go run github.com/v2fly/v2ray-core/v4/common/errors/errorgen
@@ -107,7 +108,7 @@ Loop:
 }
 
 func Decode(filename, code string) ([]byte, error) {
-	f, err := os.Open(filename)
+	f, err := filesystem.NewFileReader(filename)
 	if err != nil {
 		return nil, newError("failed to open file: ", filename).Base(err)
 	}
