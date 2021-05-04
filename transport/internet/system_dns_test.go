edff1e0eef1fb4ea2f3fdc1423ddd9e2afc73b75
rurirei <72071920+rurirei@users.noreply.github.com>
2021-05-05 02:29:33 +0800

    Create system_dns_test.go

---
+++ transport/internet/system_dns_test.go
@@ -0,0 +1,13 @@
+package internet
+
+import (
+	"context"
+	"testing"
+)
+
+func TestDNSResolver(t *testing.T) {
+	resolver := NewDNSResolver()
+	if ips, err := resolver.LookupIP(context.Background(), "ip", "www.google.com"); err != nil {
+		t.Errorf("failed to LookupIP, %v, %v", ips, err)
+	}
+}
