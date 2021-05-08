2b0c991ecb34390616179e2def299932eee41680
rurirei <72071920+rurirei@users.noreply.github.com>
2021-05-08 23:48:22 +0800

    Update sniff.go

--- common/protocol/fakedns/sniff.go
+++ common/protocol/fakedns/sniff.go
@@ -1,1 +1,32 @@
 package fakedns
+
+import (
+	"golang.org/x/net/dns/dnsmessage"
+
+	"github.com/v2fly/v2ray-core/v4/common"
+)
+
+type SniffHeader struct {
+	domain string
+}
+
+func (h *SniffHeader) Protocol() string {
+	return "fakedns"
+}
+
+func (h *SniffHeader) Domain() string {
+	return h.domain
+}
+
+func SniffFakeDNS(b []byte) (*SniffHeader, error) {
+	h := &SniffHeader{}
+
+	isIPQuery, domain, _, _ := ParseIPQuery(b)
+
+	if isIPQuery {
+		h.domain = domain
+		return h, nil
+	}
+
+	return nil, common.ErrNoClue
+}
