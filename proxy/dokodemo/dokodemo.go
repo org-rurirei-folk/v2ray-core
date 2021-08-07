5fc5a31fda69e05f57e23b43613f773e17c94862
rurirei <72071920+rurirei@users.noreply.github.com>
2021-08-07 11:34:08 +0800

    Update dokodemo.go

--- proxy/dokodemo/dokodemo.go
+++ proxy/dokodemo/dokodemo.go
@@ -89,7 +89,7 @@ func (d *Door) Process(ctx context.Context, network net.Network, conn internet.C
 	destinationOverridden := false
 	if d.config.FollowRedirect {
 		if outbound := session.OutboundFromContext(ctx); outbound != nil && outbound.TargetAddr.IsValid() {
-			dest = outbound.TargetAddr
+			dest = outbound.Target
 			destinationOverridden = true
 		} else if handshake, ok := conn.(hasHandshakeAddress); ok {
 			addr := handshake.HandshakeAddress()
