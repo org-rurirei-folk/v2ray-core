0bfc0d06409e7fa81f0ece9fc8186f5b4350cc80
rurirei <72071920+rurirei@users.noreply.github.com>
2021-08-08 18:47:18 +0800

    Update nameserver.go

--- app/dns/nameserver.go
+++ app/dns/nameserver.go
@@ -206,6 +206,7 @@ func (c *Client) QueryIP(ctx context.Context, domain string, option dns.IPOption
 	}
 
 	ctx, cancel := context.WithTimeout(ctx, 4*time.Second)
+	drfer cancel()
 	
 	ips, err := c.server.QueryIP(ctx, domain, clientIP, option, disableCache)
 	if err != nil {
