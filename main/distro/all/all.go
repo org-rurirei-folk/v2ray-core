2479c8923d7a32162a40b42f34bfb712c1c4e4ba
rurirei <72071920+rurirei@users.noreply.github.com>
2021-08-09 09:49:05 +0800

    Update all.go

--- main/distro/all/all.go
+++ main/distro/all/all.go
@@ -14,9 +14,6 @@ import (
 	_ "github.com/v2fly/v2ray-core/v4/app/proxyman/command"
 	_ "github.com/v2fly/v2ray-core/v4/app/stats/command"
 
-	// Developer preview services
-	_ "github.com/v2fly/v2ray-core/v4/app/observatory/command"
-
 	// Other optional features.
 	_ "github.com/v2fly/v2ray-core/v4/app/dns"
 	_ "github.com/v2fly/v2ray-core/v4/app/dns/fakedns"
